7946ac3c9ca3ca9d6262af6807eeb97ba03e3a0b
eryk0wski <erykdorywalski@gmail.com>
2023-08-20 12:16:40 +0200

    Gw table
    Added gameweek table and some additional coments

--- fantasy_dashboard.py
+++ fantasy_dashboard.py
@@ -30,6 +30,7 @@ league_table = league_table.drop(columns = ['rank_sort'])
 
 
 
+# adding lists to input the data from Fantasy API
 overall_rank = []
 team_value = []
 lists = {
@@ -38,12 +39,27 @@ lists = {
     'bboost': [],
     'freehit': []
 }
+
+goalkeepers_id = []
+vice_captain_id = []
+captain_id = []
+bench_points = []
 possible_chips = ['3xc','wildcard','freehit', 'bboost']
 
 #very funky code that checks for wildcards avalible for each player and their overall_rank and team value
 
 for entry in league_table['entry']:
   manager_url = 'https://fantasy.premierleague.com/api/entry/'+ str(entry) +'/history'
+  gw_players_url = 'https://fantasy.premierleague.com/api/entry/' + str(entry) + '/event/' + str(current_gw) +'/picks/'  
+  gw_players_site = requests.get(gw_players_url)
+  gw_players_data = json.loads(gw_players_site.text)  
+  goalkeepers_id.append(gw_players_data['picks'][0]['element'])
+  bench_points.append(gw_players_data['entry_history']['points_on_bench'])
+  for i in range(len(gw_players_data['picks'])):
+      if gw_players_data['picks'][i]['is_captain'] == True:
+          captain_id.append(gw_players_data['picks'][i]['element'])
+      if gw_players_data['picks'][i]['is_vice_captain'] == True:
+          vice_captain_id.append(gw_players_data['picks'][i]['element'])
   manager_site = requests.get(manager_url)
   manager_profile = json.loads(manager_site.text)
   overall_rank.append(manager_profile['current'][0]['overall_rank'])
@@ -76,6 +92,10 @@ league_table['bench_boost'] = lists['bboost']
 league_table['wildcard'] = lists['wildcard']
 league_table['free_hit'] = lists['freehit']
 league_table['tripple_captain']= lists['3xc']
+league_table['goalkeeper'] = goalkeepers_id
+league_table['bench_points'] = bench_points
+league_table['vice_captain'] = vice_captain_id
+league_table['captain'] = captain_id
 
 
 
@@ -83,7 +103,7 @@ league_table = league_table.drop(columns = ['id','entry'])
 league_table['rank_change'] = league_table['last_rank'] - league_table['rank']
 league_table = league_table.rename(columns = {"event_total":"gw_points", "total":"total_points","entry_name":"team_name"})
 
-#tried changing font colour to green if rank progress and red if you deranked
+#tried changing font colour to green if rank progress and red if you deranked that shit didnt work
 #rank_change = []
 #for rank in league_table['rack_change']:
 #  if rank >= 0:
@@ -93,7 +113,7 @@ league_table = league_table.rename(columns = {"event_total":"gw_points", "total"
 #league_table['rank_change'] = rank_change
 
 # change columns order
-league_table = league_table[['rank','rank_change','player_name', 'gw_points', 'last_rank', 'total_points', 'team_name', 'overall_rank', 'team_value', 'bench_boost', 'wildcard', 'free_hit', 'tripple_captain']]
+league_table = league_table[['rank','rank_change','player_name', 'gw_points', 'last_rank', 'total_points', 'team_name', 'overall_rank', 'team_value', 'bench_boost', 'wildcard', 'free_hit', 'tripple_captain', 'goalkeeper', 'bench_points','captain', 'vice_captain']]
 players = []
 
 #changing nick to real name if they specify otherwise
@@ -125,19 +145,23 @@ for i in range(len(league_table['rank'])):
 league_table['player'] = players
 league_table['prize'] = prizes
 
+#checking the overall percentage position in the game
 URLfunny = 'https://fantasy.premierleague.com/api/bootstrap-static/'
 page_players = requests.get(URLfunny)
 data_players = json.loads(page_players.text)
 amount_of_players =  data_players['total_players']
 league_table['top_percent[%]'] = round((100 *  league_table['overall_rank']) / amount_of_players ,1)
 
-winner_table = league_table[['rank','rank_change','player','team_name','total_points','prize']]
-teams_table = league_table[['player','gw_points','overall_rank','bench_boost', 'wildcard', 'free_hit', 'tripple_captain']]
+winner_table = league_table[['rank','rank_change','player','team_name','total_points','overall_rank','prize']]
+gw_table = league_table[['player','gw_points','goalkeeper','captain','vice_captain','bench_points']]
+teams_table = league_table[['player','bench_boost', 'wildcard', 'free_hit', 'tripple_captain']]
+
 
 #######################################################STREAMLIT#############################################################################3
 
 header = st.container()
 dataset = st.container()
+gw_stats = st.container()
 team_stats = st.container()
 
 def color_survived(val):
@@ -176,4 +200,9 @@ with team_stats:
     tittl = 'Team stats for gameweek ' + str(current_gw)
     st.title(tittl)
     st.dataframe(teams_table.style.applymap(color_survived, subset=['bench_boost', 'wildcard', 'free_hit', 'tripple_captain']))
+
+with gw_stats:
+    tittle_dummy = 'Gameweek ' + str(current_gw)
+    st.title(tittle_dummy)
+    st.dataframe(gw_table)
 
