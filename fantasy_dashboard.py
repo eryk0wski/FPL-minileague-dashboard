6923a96213ce8a7e47063e52fcd8a6ab547e40df
eryk0wski <erykdorywalski@gmail.com>
2023-08-29 12:30:42 +0200

    Player_ownership update
    Added player ownership table and player ownership chart. Also added in the bank to the gameweek table

--- fantasy_dashboard.py
+++ fantasy_dashboard.py
@@ -4,6 +4,8 @@ import requests
 import json
 import numpy as np
 import plotly.graph_objects as go
+import plotly.express as px
+from collections import Counter
 
 
 #league number
@@ -43,12 +45,17 @@ lists = {
 }
 
 goalkeepers_id = []
+in_the_bank = []
 vice_captain_id = []
 captain_id = []
 bench_points = []
 possible_chips = ['3xc','wildcard','freehit', 'bboost']
+players_ownership = []
+players_in_league = len(league_table['entry'])
 
 #very funky code that checks for wildcards avalible for each player and their overall_rank and team value
+
+
 
 for entry in league_table['entry']:
   manager_url = 'https://fantasy.premierleague.com/api/entry/'+ str(entry) +'/history'
@@ -58,6 +65,7 @@ for entry in league_table['entry']:
   goalkeepers_id.append(gw_players_data['picks'][0]['element'])
   bench_points.append(gw_players_data['entry_history']['points_on_bench'])
   for i in range(len(gw_players_data['picks'])):
+      players_ownership.append(gw_players_data['picks'][i]['element'])
       if gw_players_data['picks'][i]['is_captain'] == True:
           captain_id.append(gw_players_data['picks'][i]['element'])
       if gw_players_data['picks'][i]['is_vice_captain'] == True:
@@ -66,6 +74,7 @@ for entry in league_table['entry']:
   manager_profile = json.loads(manager_site.text)
   overall_rank.append(manager_profile['current'][current_gw - 1]['overall_rank'])
   team_value.append(manager_profile['current'][current_gw - 1]['value'])
+  in_the_bank.append(manager_profile['current'][current_gw - 1]['bank'])
   if len(manager_profile['chips']) == 0:
     lists['bboost'].append('Avalible')
     lists['3xc'].append('Avalible')
@@ -87,9 +96,12 @@ for entry in league_table['entry']:
 
 
 team_value = np.array(team_value) / 10
+in_the_bank = np.array(in_the_bank) / 10
 
+
+league_table['in_the_bank'] = in_the_bank
 league_table['overall_rank'] = overall_rank
-league_table['team_value'] = team_value
+league_table['Total_value'] = team_value
 league_table['bench_boost'] = lists['bboost']
 league_table['wildcard'] = lists['wildcard']
 league_table['free_hit'] = lists['freehit']
@@ -101,6 +113,10 @@ player_data = json.load(f)
 goalkeepers_names = []
 captain_names = []
 vice_captain_names = []
+player_ownership_names = []
+
+for player in players_ownership:
+    player_ownership_names.append(player_data[str(player)])
 
 for goalkeeper in goalkeepers_id:
     goalkeepers_names.append(player_data[str(goalkeeper)])
@@ -110,8 +126,23 @@ for captain in captain_id:
 
 for vice_captain in vice_captain_id:
     vice_captain_names.append(player_data[str(vice_captain)])
-    
+
 f.close()
+
+#adding player ownership database
+player_counts = Counter(player_ownership_names)
+
+data = {'Player_name': list(player_counts.keys()), 'Count': list(player_counts.values())}
+df_players_ownership = pd.DataFrame(data)
+#dont judge me for it
+df_players_ownership['Percent_ownership'] = round(100*(df_players_ownership['Count'] / players_in_league),2)
+df_players_ownership = df_players_ownership.sort_values(by =['Count'], ascending = False ).reset_index(drop = True)
+
+
+
+
+
+
 
 league_table['goalkeeper'] = goalkeepers_names
 league_table['bench_points'] = bench_points
@@ -134,7 +165,7 @@ league_table = league_table.rename(columns = {"event_total":"gw_points", "total"
 #league_table['rank_change'] = rank_change
 
 # change columns order
-league_table = league_table[['rank','rank_change','player_name', 'gw_points', 'last_rank', 'total_points', 'team_name', 'overall_rank', 'team_value', 'bench_boost', 'wildcard', 'free_hit', 'tripple_captain', 'goalkeeper', 'bench_points','captain', 'vice_captain']]
+league_table = league_table[['rank','rank_change','player_name', 'gw_points', 'last_rank', 'total_points', 'team_name', 'overall_rank', 'Total_value','in_the_bank', 'bench_boost', 'wildcard', 'free_hit', 'tripple_captain', 'goalkeeper', 'bench_points','captain', 'vice_captain']]
 players = []
 
 #changing nick to real name if they specify otherwise
@@ -174,7 +205,7 @@ amount_of_players =  data_players['total_players']
 league_table['top_percent[%]'] = round((100 *  league_table['overall_rank']) / amount_of_players ,1)
 
 winner_table = league_table[['rank','rank_change','player','team_name','total_points','overall_rank','prize']]
-gw_table = league_table[['player','gw_points','goalkeeper','captain','vice_captain','bench_points']]
+gw_table = league_table[['player','gw_points','goalkeeper','captain','vice_captain','bench_points','Total_value', 'in_the_bank']]
 teams_table = league_table[['player','bench_boost', 'wildcard', 'free_hit', 'tripple_captain']]
 
 #Getting the number of goalkeepers and captained
@@ -202,6 +233,7 @@ gw_table = gw_table.sort_values(by='gw_points',ascending = False).reset_index(dr
 header = st.container()
 dataset = st.container()
 gw_stats = st.container()
+col1, col2 = st.columns(2)
 captain_container = st.container()
 goalkeeper_container = st.container()
 
@@ -246,6 +278,15 @@ with gw_stats:
     st.title(tittle_dummy)
     st.dataframe(gw_table)
 
+with col1:
+    st.header('Ownership percentage')
+    st.dataframe(df_players_ownership)
+
+with col2:
+    st.header('Ownership chart')
+    figure = px.bar(df_players_ownership.head(13), x='Player_name', y='Percent_ownership',labels={'Player_name':'Player', 'Percent_ownership':'Percentage ownership[%]'})
+    st.plotly_chart(figure,use_container_width=True)
+
 with captain_container:
     st.header("Capitained")
     labels = u_captained
@@ -253,6 +294,7 @@ with captain_container:
     fig = go.Figure(data=[go.Pie(labels=labels, values=values)])
     fig.update_layout()
     st.plotly_chart(fig,use_container_width=False)
+    
 with goalkeeper_container:
     st.header("Goalkeepers")
     labels = u_goalkeepers
